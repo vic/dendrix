540854e5f3eb6289ead172179538af5ce2f583d8
Victor Borja <vborja@apache.org>
2025-06-30 23:37:09 -0600

    Add update sources action

---
+++ dev/modules/gh-update-sources.nix
@@ -0,0 +1,66 @@
+let
+  name = "update-sources.yaml";
+  path_ = ".github/workflows/${name}";
+in
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      files.files = [
+        {
+          inherit path_;
+          drv = pkgs.writers.writeJSON name {
+            name = "update sources";
+            on = {
+              workflow_call = { };
+              workflow_dispatch = { };
+              repository_dispatch = { };
+              schedule = {
+                cron = "0 0 * * *";
+              };
+            };
+            permissions = {
+              contents = "write";
+              pull-requests = "write";
+              id-token = "write";
+            };
+            concurrency = {
+              group = "update";
+              cancel-in-progress = true;
+            };
+            jobs.check = {
+              environment.name = "update-sources";
+              environment.url = "\${{steps.update.outputs.pull-request-url}}";
+              runs-on = "ubuntu-latest";
+              steps = (import ./_gh-provision-nix.nix) ++ [
+                {
+                  run = "nix develop --accept-flake-config --override-input dendrix . --print-build-logs ./dev -c pins update";
+                }
+                {
+                  run = "nix develop --accept-flake-config --override-input dendrix . --print-build-logs ./dev -c discover";
+                }
+                {
+                  run = "nix flake --accept-flake-config check path:dev --override-input dendrix . --print-build-logs";
+                }
+                {
+                  id = "update";
+                  uses = "peter-evans/create-pull-request@v7";
+                  "with" = {
+                    author = "Victor Borja <vborja@apache.org>";
+                    commit-message = "Update sources";
+                    branch = "update-sources";
+                    branch-suffix = "timestamp";
+                    title = "Update community sources.";
+                    body = "Please review.";
+                    assignees = "vic,copilot";
+                    reviewers = "vic,copilot";
+                  };
+                }
+              ];
+            };
+          };
+        }
+      ];
+      treefmt.settings.global.excludes = [ path_ ];
+    };
+}
